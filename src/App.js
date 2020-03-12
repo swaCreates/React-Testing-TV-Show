cfe8e87b06c0814e6793fe21346c2ded29db8ec6
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-03-11 20:05:16 -0400

    transferred fetchShow func / created test folder

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
+import {fetchShow} from './api/fetchShow';
 
 import { formatSeasons } from "./utils/formatSeasons";
 
@@ -14,18 +15,14 @@ export default function App() {
   const [selectedSeason, setSelectedSeason] = useState("");
   const episodes = seasons[selectedSeason] || [];
 
+
+  //this was changed to use fetchShow from api folder
+  // acts just like an api call
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
+    fetchShow().then(res => {
+      setShow(res.data);
+      setSeasons(formatSeasons(res.data._embedded.episodes));
+    });
   }, []);
 
   const handleSelect = e => {
