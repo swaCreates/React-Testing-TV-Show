1e7e6a8645e5c9a02cdf1de451fb95cddc68d959
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-03-13 20:07:04 -0400

    test working

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -6,15 +6,7 @@ test('App renders without crashing', () => {
     render(<App />);
 })
 
-test('dropdown', () => {
-    const {getByTestId} = render(<App />);
 
-    const dropdown= getByTestId('dropdown');
-
-    const {getByText} = dropdown;
-
-    getByText(/search a season/i);
-})
 
 
 
